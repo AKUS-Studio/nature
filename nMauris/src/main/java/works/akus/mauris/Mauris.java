1ff4d5915c78b1830b6ac832e6fced6918c1d47b
RuverQ <ruqalex@gmail.com>
2022-10-13 22:24:09 +0300

    nMauris | Config Default

--- nMauris/src/main/java/works/akus/mauris/Mauris.java
+++ nMauris/src/main/java/works/akus/mauris/Mauris.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import works.akus.mauris.commands.CommandManager;
 import works.akus.mauris.things.ThingManager;
 
+import java.io.File;
+
 public class Mauris extends JavaPlugin {
 
 	static Mauris instance;
@@ -14,11 +16,23 @@ public class Mauris extends JavaPlugin {
 	public void onEnable() {
 		instance = this;
 
+		//Managers Setup
 		thingManager = new ThingManager();
 		thingManager.setUp();
 
 		commandManager = new CommandManager();
 		commandManager.setUp();
+		//
+
+		createConfig();
+	}
+
+	private void createConfig(){
+		File config = new File(getDataFolder() + File.separator + "config.yml");
+		if (!config.exists()) {
+			getConfig().options().copyDefaults(true);
+			saveDefaultConfig();;
+		}
 	}
 
 	public ThingManager getThingManager() {
