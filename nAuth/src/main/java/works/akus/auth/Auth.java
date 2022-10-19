560ecf07e0d367ef5d91e1c59dc4eec245d51b16
RuverQ <ruqalex@gmail.com>
2022-10-19 14:57:36 +0300

    nAuth implementation

---
+++ nAuth/src/main/java/works/akus/auth/Auth.java
@@ -0,0 +1,18 @@
+package works.akus.auth;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Auth extends JavaPlugin {
+
+
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
